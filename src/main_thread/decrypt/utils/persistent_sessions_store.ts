11c4c1790160fac1f72ad3e518fb444eb54e04f5
Paul Berberian <paul.berberian@proton.me>
2025-04-02 17:52:18 +0200

    wip

--- src/main_thread/decrypt/utils/persistent_sessions_store.ts
+++ src/main_thread/decrypt/utils/persistent_sessions_store.ts
@@ -232,6 +232,7 @@ export default class PersistentSessionsStore {
    * @returns {number}
    */
   private _getIndex(initData: IProcessedProtectionData): number {
+    console.warn("!!!!!! _getIndex", this._entries.length);
     // Older versions of the format include a concatenation of all
     // initialization data and its hash.
     // This is only computed lazily, the first time it is needed.
@@ -250,8 +251,15 @@ export default class PersistentSessionsStore {
       return lazyConcatenatedData;
     }
 
+    console.warn(
+      "!!!!!! initData",
+      "type",
+      initData.type,
+      initData.values.getFormattedValues().length,
+    );
     for (let i = 0; i < this._entries.length; i++) {
       const entry = this._entries[i];
+      console.warn("!!!!! ENTRY", JSON.stringify(entry));
       if (entry.initDataType === initData.type) {
         switch (entry.version) {
           case 4:
