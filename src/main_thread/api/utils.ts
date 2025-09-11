2d401c8d1445d704b7f95e8557b5e20c15dcdc50
Paul Berberian <paul.berberian@proton.me>
2025-09-11 17:44:44 +0200

    Revert "review feedback"
    This reverts commit 76f635963daf39fcde59afa5ce0141ab7e17be65.

--- src/main_thread/api/utils.ts
+++ src/main_thread/api/utils.ts
@@ -15,7 +15,7 @@
  */
 
 import type { IMediaElement } from "../../compat/browser_compatibility_types";
-import canPreloadBeforePlay from "../../compat/can_preload_before_play";
+import treatNotReadyAsLoaded from "../../compat/tread_not_ready_as_loaded";
 import config from "../../config";
 import type {
   IPlaybackObservation,
@@ -244,11 +244,13 @@ export function getLoadedContentState(
       return PLAYER_STATES.FREEZING;
     }
 
-    if (stalledStatus === "not-ready" && !canPreloadBeforePlay(isDirectFile)) {
+    if (
+      (stalledStatus === "not-ready" || stalledStatus === "internal-seek") &&
+      treatNotReadyAsLoaded(isDirectFile)
+    ) {
       /**
        * On some devices, `readyState` remains low until `play()` is called,
-       * meaning that no media data can be pre-loaded.
-       * Treat it as loaded in this case.
+       * even though the media is effectively ready. Treat it as loaded in this case.
        */
       return PLAYER_STATES.LOADED;
     }
