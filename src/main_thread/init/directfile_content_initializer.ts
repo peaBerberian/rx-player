3045a4b2bf862c1bc639c0e20c31cdd4b3a51194
Paul Berberian <paul.berberian@proton.me>
2025-09-11 17:41:36 +0200

    Revert "review"
    This reverts commit 89b6610330b376ff9cfaf126416b43c58e60e40d.

--- src/main_thread/init/directfile_content_initializer.ts
+++ src/main_thread/init/directfile_content_initializer.ts
@@ -276,7 +276,6 @@ export function setAutoplay(
     return;
   }
   const autoplayPreviousValue = mediaElement.autoplay;
-  mediaElement.autoplay = autoplay;
   cancellationSignal.register(() => {
     /**
      * Restore the `autoplay` attribute to its previous value.
@@ -285,6 +284,7 @@ export function setAutoplay(
      */
     mediaElement.autoplay = autoplayPreviousValue;
   });
+  mediaElement.autoplay = autoplay;
 }
 
 /**
