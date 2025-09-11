2a3c0df63a2239b668cc797bfe69ab09ddbc8d32
Paul Berberian <paul.berberian@proton.me>
2025-09-11 17:43:16 +0200

    Revert "fix: take into consideration the previous to avoid getting back in LOADED state after a buffering"
    This reverts commit b0b5eb319d872535ea06195f2f8b8b618362c7e6.

--- src/main_thread/api/utils.ts
+++ src/main_thread/api/utils.ts
@@ -129,25 +129,13 @@ export function constructPlayerStateReference(
       if (playerStateRef.getValue() === PLAYER_STATES.LOADING) {
         playerStateRef.setValue(PLAYER_STATES.LOADED);
         if (!cancelSignal.isCancelled()) {
-          const newState = getLoadedContentState(
-            mediaElement,
-            null,
-            isDirectFile,
-            playerStateRef.getValue(),
-          );
+          const newState = getLoadedContentState(mediaElement, null, isDirectFile);
           if (newState !== PLAYER_STATES.PAUSED) {
             playerStateRef.setValue(newState);
           }
         }
       } else if (playerStateRef.getValue() === PLAYER_STATES.RELOADING) {
-        playerStateRef.setValue(
-          getLoadedContentState(
-            mediaElement,
-            null,
-            isDirectFile,
-            playerStateRef.getValue(),
-          ),
-        );
+        playerStateRef.setValue(getLoadedContentState(mediaElement, null, isDirectFile));
       } else {
         updateStateIfLoaded(null);
       }
@@ -205,12 +193,7 @@ export function constructPlayerStateReference(
     if (!isLoadedState(playerStateRef.getValue())) {
       return;
     }
-    const newState = getLoadedContentState(
-      mediaElement,
-      stallRes,
-      isDirectFile,
-      playerStateRef.getValue(),
-    );
+    const newState = getLoadedContentState(mediaElement, stallRes, isDirectFile);
     const prevState = playerStateRef.getValue();
 
     // Some safety checks to avoid having nonsense state switches
@@ -233,7 +216,6 @@ export function getLoadedContentState(
   mediaElement: IMediaElement,
   stalledStatus: IStallingSituation | null,
   isDirectFile: boolean,
-  previousState: IPlayerState,
 ): IPlayerState {
   const { FORCED_ENDED_THRESHOLD } = config.getCurrent();
   if (mediaElement.ended) {
@@ -262,12 +244,11 @@ export function getLoadedContentState(
       return PLAYER_STATES.FREEZING;
     }
 
-    if (previousState === PLAYER_STATES.LOADED && !canPreloadBeforePlay(isDirectFile)) {
+    if (stalledStatus === "not-ready" && !canPreloadBeforePlay(isDirectFile)) {
       /**
-       * On devices that do not support preloading, a data buffer cannot be constructed.
-       * Normally, this situation would trigger the BUFFERING state. However, since these devices
-       * are unable to buffer data, having low or no data is expected while waiting for a play() call.
-       * Therefore, in this case, we remain in the LOADED state instead of transitioning to BUFFERING.
+       * On some devices, `readyState` remains low until `play()` is called,
+       * meaning that no media data can be pre-loaded.
+       * Treat it as loaded in this case.
        */
       return PLAYER_STATES.LOADED;
     }
